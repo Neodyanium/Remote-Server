8e4a1ccf2d0c216d7e769f8a1ebb945147143d2c
Sangharsh <sangharsh_pipare@persistent.com>
2022-01-29 10:31:48 +0530

    Added Function Headers

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+#Add Implementation
 def add(x,y):
 	pass
+#Sub Implementation
 def subtract(x,y):
     pass
+#Mult Implementation
 def multiply(x,y):
 	pass
+#Div Implementation
 def divide(x,y):
     pass
